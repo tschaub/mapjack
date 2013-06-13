96d6895b056c4709d5d252d43458c05d81ea51ff
Tim Schaub <tim.schaub@gmail.com>
2013-06-13 15:18:33 -0600

    Onward

---
+++ gruntfile.js
@@ -0,0 +1,20 @@
+
+
+/**
+ * Grunt config.
+ * @param  {Object} grunt Grunt itself.
+ */
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    'gh-pages': {
+      src: ['readme.md']
+    }
+  });
+
+};
+
+
+grunt.loadNpmTasks('grunt-gh-pages');
+
+grunt.registerTask('default', ['gh-pages']);
