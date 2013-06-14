8d3fd710a476000bbdf10d6840343ca036decbae
Tim Schaub <tim.schaub@gmail.com>
2013-06-14 02:59:04 -0600

    Updates

--- .grunt/mapjack/dist/mapjack.user.js
+++ .grunt/mapjack/dist/mapjack.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name mapjack
 // @namespace http://tschaub.net/mapjack/
-// @version 0.0.0
+// @version 0.0.1
 // @description Map based editing of GeoJSON files on GitHub.
 // @match https://github.com/*/*/edit/*/*.geojson
 // @copyright 2013+, Tim Schaub
